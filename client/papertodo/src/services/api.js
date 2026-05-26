9c49315f43483207688b00c6c30cfb43638dea0d
vrkljam <vrkljam@earthlink.net>
2026-05-26 11:55:29 -0600

    changed to VITE in api.js

--- client/papertodo/src/services/api.js
+++ client/papertodo/src/services/api.js
@@ -1,7 +1,7 @@
 import axios from "axios";
 
 const API = axios.create({
-  baseURL: process.env.REACT_APP_BACKEND_URL || "http://localhost:5600/api",
+  baseURL: import.meta.env.VITE_APP_BACKEND_URL || "http://localhost:5600/api",
 });
 
 export default API;
